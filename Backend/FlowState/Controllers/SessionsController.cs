5b40e928f559016dfd699690ca2832feec6923eb
bzaricki <bzaricki56@gmail.com>
2026-05-29 21:08:20 -0400

    Add project files.

---
+++ Backend/FlowState/Controllers/SessionsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowState.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SessionsController : ControllerBase
+    {
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok(new { status = "Flow State API is running" });
+        }
+    }
+}
